7bf84b6e6906f35d6df63478ce34f50db5878207
Kinokino00 <ichiki.ichiki@gmail.com>
2025-05-22 22:06:30 +0800

    fix: change images path

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -74,68 +74,68 @@
       </div>
       <div class="grid gap-1 ml-4 md:grid-cols-2 2xl:gap-3">
         <div class="flex items-center gap-1 md:hidden">
-          <img class="icon" :src="`@/assets/images/Java.png`">
+          <img class="icon" src="@/assets/images/Java.png">
           <p>Java</p>
         </div>
         <div class="flex items-center gap-1 md:hidden">
-          <img class="icon" :src="`@/assets/images/Vue.png`">
+          <img class="icon" src="@/assets/images/Vue.png">
           <p>Vue</p>
         </div>
         <div class="flex items-center gap-1 md:hidden">
-          <img class="icon" :src="`@/assets/images/Python.png`">
+          <img class="icon" src="@/assets/images/Python.png">
           <p>Python</p>
         </div>
         <div class="flex items-center gap-1 md:hidden">
-          <img class="icon" :src="`@/assets/images/Tailwindcss.png`">
+          <img class="icon" src="@/assets/images/Tailwindcss.png">
           <p>Tailwindcss</p>
         </div>
         <div class="flex items-center gap-1 md:hidden">
-          <img class="icon" :src="`@/assets/images/Selenium.png`">
+          <img class="icon" src="@/assets/images/Selenium.png">
           <p>Selenium</p>
         </div>
         <div class="flex items-center gap-1 md:hidden">
-          <img class="icon" :src="`@/assets/images/Typescript.png`">
+          <img class="icon" src="@/assets/images/Typescript.png">
           <p>Typescript</p>
         </div>
         <div class="flex items-center gap-1 md:hidden">
-          <img class="icon" :src="`@/assets/images/BeautifulSoup.png`">
+          <img class="icon" src="@/assets/images/BeautifulSoup.png">
           <p>BeautifulSoup</p>
         </div>
         <div class="flex items-center gap-1 md:hidden">
-          <img class="icon" :src="`@/assets/images/Javascript.png`">
+          <img class="icon" src="@/assets/images/Javascript.png">
           <p>Javascript</p>
         </div>
 
         <div class="hidden items-center gap-1 md:flex">
-          <img class="icon" :src="`@/assets/images/Java.png`">
+          <img class="icon" src="@/assets/images/Java.png">
           <p>Java</p>
         </div>
         <div class="hidden items-center gap-1 md:flex">
-          <img class="icon" :src="`@/assets/images/Python.png`">
+          <img class="icon" src="@/assets/images/Python.png">
           <p>Python</p>
         </div>
         <div class="hidden items-center gap-1 md:flex">
-          <img class="icon" :src="`@/assets/images/Selenium.png`">
+          <img class="icon" src="@/assets/images/Selenium.png">
           <p>Selenium</p>
         </div>
         <div class="hidden items-center gap-1 md:flex">
-          <img class="icon" :src="`@/assets/images/BeautifulSoup.png`">
+          <img class="icon" src="@/assets/images/BeautifulSoup.png">
           <p>BeautifulSoup</p>
         </div>
         <div class="hidden items-center gap-1 md:flex">
-          <img class="icon" :src="`@/assets/images/Vue.png`">
+          <img class="icon" src="@/assets/images/Vue.png">
           <p>Vue</p>
         </div>
         <div class="hidden items-center gap-1 md:flex">
-          <img class="icon" :src="`@/assets/images/Tailwindcss.png`">
+          <img class="icon" src="@/assets/images/Tailwindcss.png">
           <p>Tailwindcss</p>
         </div>
         <div class="hidden items-center gap-1 md:flex">
-          <img class="icon" :src="`@/assets/images/Typescript.png`">
+          <img class="icon" src="@/assets/images/Typescript.png">
           <p>Typescript</p>
         </div>
         <div class="hidden items-center gap-1 md:flex">
-          <img class="icon" :src="`@/assets/images/Javascript.png`">
+          <img class="icon" src="@/assets/images/Javascript.png">
           <p>Javascript</p>
         </div>
       </div>
